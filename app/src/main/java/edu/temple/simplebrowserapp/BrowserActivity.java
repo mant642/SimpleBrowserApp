a6a8fbb240b6682a8ea3e09809a280a8d870ad9f
Mathew Anthony <tuf62412@temple.edu>
2020-10-27 16:36:05 -0400

    Further foundational stuff. Properly formatted layouts, and tested online functionality by adding hardcoded WebView to PageViewerFragment.

--- app/src/main/java/edu/temple/simplebrowserapp/BrowserActivity.java
+++ app/src/main/java/edu/temple/simplebrowserapp/BrowserActivity.java
@@ -1,6 +1,8 @@
 package edu.temple.simplebrowserapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
 
@@ -10,5 +12,16 @@ public class BrowserActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+
+        PageControlFragment f1 = new PageControlFragment();
+        PageViewerFragment f2 = new PageViewerFragment();
+
+        ft.replace(R.id.page_control, f1);
+        ft.replace(R.id.page_viewer, f2);
+
+        ft.commit();
     }
 }
